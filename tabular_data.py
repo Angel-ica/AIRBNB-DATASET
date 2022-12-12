69e8f48239b3e1e4fb408f97ce11e8f14bd99de2
Angelica Aluo <angelicaaluo@Angelicas-MacBook-Air.local>
2022-12-12 00:53:29 +0000

    data cleaning

--- tabular_data.py
+++ tabular_data.py
@@ -1,21 +1,30 @@
+#import necessary libraries
 import pandas as pd
 import numpy as np
-from openpyxl.workbook import Workbook
-import itertools
-import boltons.iterutils as bi 
-pd.options.mode.use_inf_as_na = True
+# import boltons.iterutils as bi 
+# pd.options.mode.use_inf_as_na = True
+names=['ID', 'Category', 'Title', 'Description','Amenities','Location','Guests','Beds','Bathrooms','Price_Night','Cleanliness_rate','Accuracy_rate','Location_rate','Check-in-rate','Value_rate','Amenities_count','Url','Bedrooms']
+
+df=pd.read_csv('~/Airbnb/AIRBNB-DATASET/airbnb-property-listings/tabular_data/listing.csv',header=None,names=names)
+
+#task is to remove the rows with missing values in the ratings columns
 def remove_rows_with_missing_ratings():
-    names=["ID", "Category", 'Title', 'Description','Amenities','Location','Guests','Beds','Bathrooms','Price_Night','Cleanliness_rate','Accuracy_rate','Location_rate','Check-in-rate','Value_rate','Amenities_count','Url','Bedrooms']
-
-    df=pd.read_csv('~/Airbnb/AIRBNB-DATASET/airbnb-property-listings/tabular_data/listing.csv',header=None,names=names, na_values='""')
     print(df.info())
-    # print(df)
-    print(np.sort(df.isnull()))
-    for row in df['Value_rate']:
-        if np.any(df['Value_rate'].isnull())==True:
-            df.drop(row)
-    print(df.info())
+    df.replace('', np.nan,inplace=True)
+    df.dropna(subset=['Accuracy_rate','Cleanliness_rate','Location_rate','Value_rate','Check-in-rate'],inplace=True)
+    df.to_json('dataset.json')
+    print('done')
+    return df
 
-    # new_dict=bi.remap(df_dict.values,({key: value is not 'nan' for key, value in df_dict.values}))
+# def combine_description_strings():
+#     # listOfStrings=df['Description']
+#     # for row in df['Description'].values:
+#     #     ''.join(row) 
+#     #     return row
+#     print(df['Description'].values)
+# # listOfStrings=df['Description']
+# # combine_description_strings(listOfStrings)
+# print(combine_description_strings())
+
 
 remove_rows_with_missing_ratings()
