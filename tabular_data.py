971a4b5af880bc7a330636a8209f46b5a8cceced
Angelica Aluo <angelicaaluo@Angelicas-MacBook-Air.local>
2022-12-14 02:50:48 +0000

    data cleaning

--- tabular_data.py
+++ tabular_data.py
@@ -1,30 +1,32 @@
-#import necessary libraries
 import pandas as pd
 import numpy as np
 # import boltons.iterutils as bi 
-# pd.options.mode.use_inf_as_na = True
-names=['ID', 'Category', 'Title', 'Description','Amenities','Location','Guests','Beds','Bathrooms','Price_Night','Cleanliness_rate','Accuracy_rate','Location_rate','Check-in-rate','Value_rate','Amenities_count','Url','Bedrooms']
 
-df=pd.read_csv('~/Airbnb/AIRBNB-DATASET/airbnb-property-listings/tabular_data/listing.csv',header=None,names=names)
 
-#task is to remove the rows with missing values in the ratings columns
-def remove_rows_with_missing_ratings():
-    print(df.info())
+def remove_rows_with_missing_rating():
+    # print(df.info())
     df.replace('', np.nan,inplace=True)
-    df.dropna(subset=['Accuracy_rate','Cleanliness_rate','Location_rate','Value_rate','Check-in-rate'],inplace=True)
-    df.to_json('dataset.json')
-    print('done')
+    df.dropna(subset=['Accuracy_rating','Cleanliness_rating','Location_rating','Value_rating','Check-in_rating'],inplace=True)
     return df
 
-# def combine_description_strings():
-#     # listOfStrings=df['Description']
-#     # for row in df['Description'].values:
-#     #     ''.join(row) 
-#     #     return row
-#     print(df['Description'].values)
-# # listOfStrings=df['Description']
-# # combine_description_strings(listOfStrings)
-# print(combine_description_strings())
+def combine_description_strings():
+    df['Description'] = df['Description'].str.strip("[]").str.replace(",","").str.replace('\"','').str.replace("\'","").str.strip('About this space')
+    print(df['Description'])
 
+def set_feature_default_values():
+    replace = ["guests", "beds", "bathrooms", "bedrooms"]
+    df[replace] = df[replace].fillna(1)
+    # print(df['bathrooms'])
+    return df
 
-remove_rows_with_missing_ratings()
+def clean_tabular_data():
+    remove_rows_with_missing_rating()
+    combine_description_strings()
+    set_feature_default_values()
+    return df
+
+if __name__=='__main__':
+    df=pd.read_csv('~/Airbnb/AIRBNB-DATASET/airbnb-property-listings/tabular_data/listing.csv')
+    clean_df=clean_tabular_data()
+    clean_df.to_csv('~/Airbnb/AIRBNB-DATASET/airbnb-property-listings/tabular_data/clean_data.csv')
+    
